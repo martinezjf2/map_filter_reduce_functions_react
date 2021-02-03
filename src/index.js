95b0eabbc4b5ded1e640709745d4e488b2dd8178
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-02-02 21:58:20 -0500

    using reduce to add everything within the array

--- src/index.js
+++ src/index.js
@@ -23,6 +23,12 @@ var numbers = [3, 56, 2, 48, 5];
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
+var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+  return accumulator + currentNumber;
+});
+
+console.log(newNumber);
+
 //Find - find the first item that matches from an array.
 
 //FindIndex - find the index of the first item that matches.
